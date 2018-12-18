9730b0b0fe372e7f0ca7a42eb6bbe5fed0d8a03f
Ted Kim <tedkim97@gmail.com>
2018-12-18 16:59:19 -0600

    Decided to moe some of the connection functions inside the crawler class
    Working on authentication outside of the code

--- webscrapper/email_utility.py
+++ webscrapper/email_utility.py
@@ -3,3 +3,26 @@ import email
 import os
 import cred 
 
+def extract_emails(byte_data):
+	msgs = []
+	for num in byte_data[0].split():
+		ty, data = con.fetch(num,'(RFC822)')
+		msgs.append(data)
+	return msgs
+
+
+
+class email_authenticator():
+	def __init__(self,username,password,imap_url):
+		self.connection = imaplib.IMAP4_SSL(imap_url)
+		self.connection.login(username,password)
+
+	def search_email(self,key,value):
+		
+		
+
+
+
+if __name__ == "__main__":
+	username = cred.email_un 
+	password = cred.email_pd
--- webscrapper/sel_crawler.py
+++ webscrapper/sel_crawler.py
@@ -4,35 +4,85 @@ import time
 import cred #python file that contains venmo login information
 #This is excluded from the github commit for obvious reasons
 
-def open_website(driv_arg,url,x=0,y=0,width=900,length=900):
-	driv_arg.get(url)
-	driv_arg.set_window_size(width,length)
-	driv_arg.set_window_position(x,y)
+# def open_website(driv_arg,url,x=0,y=0,width=900,length=900):
+# 	driv_arg.get(url)
+# 	driv_arg.set_window_size(width,length)
+# 	driv_arg.set_window_position(x,y)
 		
-def login(driv_arg,username,password,**html_resc):
-	uname_ele = html_resc['username_name_element']
-	pword_ele = html_resc['password_name_element']
-	button_ele = html_resc['button_name_element']
+# def login(driv_arg,username,password,**html_resc):
+# 	print(html_resc)
+# 	uname_ele = html_resc['username_name_element']
+# 	pword_ele = html_resc['password_name_element']
+# 	button_ele = html_resc['button_name_element']
 
-	unname_html = driv_arg.find_elements_by_class_name(uname_ele)
-	pword_html = driv_arg.find_element_by_name(pword_ele)
-	uname_html.send_keys(username)
-	pword_html.send_keys(password)
+# 	uname_html = driv_arg.find_element_by_name(uname_ele)
+# 	pword_html = driv_arg.find_element_by_name(pword_ele)
+# 	uname_html.send_keys(username)
+# 	pword_html.send_keys(password)
 
-	login_button = driv_arg.find_elements_by_class_name(button_ele)
-	login_button.click()
+# 	login_button = driv_arg.find_element_by_class_name(button_ele)
+	# login_button.click()
 
 class venmo_crawler():
 	def __init__(self,username,password,**html_resources):
 		self.resc = html_resources
 		self.driver = webdriver.Chrome()
-		open_website(self.driver, self.resc['login-url'])
-		login(self.driver, username, password)
+		self.open_website()
+		self.login(username,password)
+		# open_website(self.driver, self.resc['login-url'])
+		# login(self.driver, username, password,**self.resc)
+		self.click_send_authentication_code()
+		self.enter_authentication_code(123456)
 
-	def send_authentication_code(self):
+	def open_website(self, x=0,y=0,width=900,length=900):
+		self.driver.get(self.resc['login-url'])
+		self.driver.set_window_size(width,length)
+		self.driver.set_window_position(x,y)
+		print("Opening website")
+		self.pause_crawler(5)
+		return;
+
+	def login(self,username,password):
+		uname_ele = self.resc['username_name_element']
+		pword_ele = self.resc['password_name_element']
+		button_ele = self.resc['button_name_element']
+
+		uname_html = self.driver.find_element_by_name(uname_ele)
+		pword_html = self.driver.find_element_by_name(pword_ele)
+		uname_html.send_keys(username)
+		pword_html.send_keys(password)
+
+		login_button = self.driver.find_element_by_class_name(button_ele)
+		login_button.click()
+		print("Logging in")
+		self.pause_crawler(10)
+		return;
+
+	def click_send_authentication_code(self):
+		button_send_code = self.driver.find_element_by_class_name(
+											self.resc['button_name_element'])
+		button_send_code.click()
+		print("clicked the 'Send authentication code'")
+		self.pause_crawler(10)
+		return;
+
+	def enter_authentication_code(self, code:int):
+		auth_code = self.driver.find_element_by_name(self.resc['auth-code'])
+		auth_code.send_keys("{}".format(code))
+
+		button_submit_auth = self.driver.find_element_by_class_name(
+										self.resc['button_name_element'])
+		button_submit_auth.click()
 		
+		self.pause_crawler(20)
+		button_remember_me = self.driver.find_elements_by_class_name(
+										self.resc['button_name_element'])
+		button_remember_me[0].click()
+		self.pause_crawler(10)
+		return;
 
-	def pause_crawler(sec, verbose= True):
+
+	def pause_crawler(self,sec, verbose= True):
 		if(verbose):
 			print("Sleeping for {}".format(sec))
 			time.sleep(sec)
@@ -45,6 +95,7 @@ if __name__ == "__main__":
 		'login-url': 'https://venmo.com/account/sign-in/',
 		'username_name_element':'phoneEmailUsername',
 		'password_name_element':'password',
-		'button_name_element':'ladda-label'
+		'button_name_element':'ladda-label',
+		'auth-code':'token'
 	}
 	a = venmo_crawler(cred.username,cred.password,**html_info)
